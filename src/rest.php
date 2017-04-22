6812bf2f6412507b1729c95c5b019dc7acd73daa
Bartosz Michalski <bartoszmichalski@poczta.fm>
2017-04-22 18:00:29 +0200

    formating code

--- src/rest.php
+++ src/rest.php
@@ -2,77 +2,90 @@
 //include_once 'connection.php';
 require_once 'config.php';
 
-class Book implements JsonSerializable {
+class Book implements JsonSerializable
+{
     public $id;
     public $title;
     public $author;
 
-    public $books= [];
+    public $books = [];
     
-    public function __construct() {
+    public function __construct()
+    {
         $this->id = -1;
-        $this->author='';
-        $this->title='';
+        $this->author ='';
+        $this->title ='';
     }
     
-    public function getId () {
+    public function getId ()
+    {
         return $this->id;
     }
-    public function getTitle () {
+    
+    public function getTitle ()
+    {
         return $this->title;
     }
-    public function getAuthor () {
+    
+    public function getAuthor ()
+    {
         return $this->author;
     }
-    public function setTitle ($title) {
-        $this->title=$title;
+    
+    public function setTitle ($title)
+    {
+        $this->title = $title;
         return $this;
     }
-    public function setAuthor ($author) {
-        $this->author=$author;
+    
+    public function setAuthor ($author)
+    {
+        $this->author = $author;
         return $this;
     }
-    //Funkcję loadFromDB(conn, id).
-    static function loadBookFromDBById($connection, $id) {
-       $sql=sprintf("SELECT `id`, `title`, `author` FROM `books` WHERE id=%d", $id);
+    
+    static function loadBookFromDBById($connection, $id)
+    {
+       $sql = sprintf("SELECT `id`, `title`, `author` FROM `books` WHERE id=%d", $id);
        
-        $result =$connection->query($sql);
-        if ($result==true && $result->num_rows ==1) {
-        //   foreach($result as $row) {
-                $row =$result->fetch_assoc();
-                $loadedBook = new Book();
-                $loadedBook->id = $row['id'];
-                $loadedBook->title=$row['title'];
-                $loadedBook->author=$row['author'];
-                
-             //   $ret[]=$loadedTweet;
-                return $loadedBook;
-           // }
+        $result = $connection->query($sql);
+        if ($result == true && $result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $loadedBook = new Book();
+            $loadedBook->id = $row['id'];
+            $loadedBook->title = $row['title'];
+            $loadedBook->author = $row['author'];
+
+            return $loadedBook;
         }
-        return null;
         
+        return null;
     }
+    
     public function saveBookInDB(mysqli $connection)
     {
-        if ($this->id==-1) {
+        if ($this->id == -1) {
             $sql=  sprintf("INSERT INTO `books`(`id`, `title`, `author`) VALUES (NULL,'%s','%s')",
                             $this->title,
                             $this->author);
             $result = $connection->query($sql);
-            if ($result==true) {
-                $this->id=$connection->insert_id;
+            if ($result == true) {
+                $this->id = $connection->insert_id;
                 return TRUE;
             }
         } else {
-            $sql= sprintf("UPDATE `books` SET `title`='%s',`author`='%s' WHERE id=%d", $this->title, $this->author, $this->id);
+            $sql = sprintf("UPDATE `books` SET `title`='%s',`author`='%s' WHERE id=%d", $this->title, $this->author, $this->id);
             $result = $connection->query($sql);
             if($result == true){
-            return true;
+                return true;
             }
         }
+        
         return false;
     }
-    public function deleteBookFromDB(mysqli $connection){
+    
+    public function deleteBookFromDB(mysqli $connection)
+    {
         if($this->id != -1){
             $sql = sprintf("DELETE FROM `books` WHERE id=%d",$this->id);
             $result = $connection->query($sql);
@@ -80,40 +93,42 @@ class Book implements JsonSerializable {
                 $this->id = -1;
                 return true;
             }
-        return false;
-
+            
+            return false;
         }
  
         return true;
     }
-    static function loadAllBooks(mysqli $connection) {
+    
+    static function loadAllBooks(mysqli $connection)
+    {
         $sql="SELECT * FROM books";
         $ret = [];
-        $result =$connection->query($sql);
-        if ($result == true && $result->num_rows!=0) {
+        $result = $connection->query($sql);
+        if ($result == true && $result->num_rows != 0) {
             foreach($result as $row) {
                 $loadedBook = new Book ();
                 $loadedBook->id = $row['id'];
-                $loadedBook->title=$row['title'];
-                $loadedBook->author=$row['author'];
-                $books[]=$loadedBook;
+                $loadedBook->title = $row['title'];
+                $loadedBook->author = $row['author'];
+                $books[] = $loadedBook;
             }
-        return $books;    
+            return $books;    
         }
       
-        return null;
-        
+        return null;        
     }
-    public function jsonSerialize() {
+    
+    public function jsonSerialize()
+    {
         if(count($this->books)){
             $books = [];
             foreach($this->books as $book){
-                $books[]=$this->getBookAsTable($book);
+                $books[] = $this->getBookAsTable($book);
             }
             return $books;
-        }else{
-            return $this->getBookAsTable($this);
-            
+        } else {
+            return $this->getBookAsTable($this);            
         }
     }
     
@@ -124,18 +139,15 @@ class Book implements JsonSerializable {
                 'author'=> $bookObj->author
             ];
     }
-
 }
-
-//header('Content-Type:application/json; charset=UTF-8');
 
 switch($_SERVER['REQUEST_METHOD']){
     
     case 'POST':
          //   add action
-        if (isset($_POST['title']) && isset($_POST['author']) && $_POST['title']!='' && $_POST['author']!='') {
-            $title=$_POST['title'];
-            $author=$_POST['author'];
+        if (isset($_POST['title']) && isset($_POST['author']) && $_POST['title'] != '' && $_POST['author'] != '') {
+            $title = $_POST['title'];
+            $author = $_POST['author'];
             $newBook = new Book();
             $newBook->setTitle($title);
             $newBook->setAuthor($author);
@@ -150,40 +162,39 @@ switch($_SERVER['REQUEST_METHOD']){
         break;
     
     case 'GET':
-            // get action
-            if(isset($_GET['id'])){   // load book by id
-                $loadedBook=Book::loadBookFromDBById($mysql,$_GET['id']);
-                echo json_encode($loadedBook);
-            } else {
+        // get action
+        if(isset($_GET['id'])){   // load book by id
+            $loadedBook = Book::loadBookFromDBById($mysql,$_GET['id']);
+            echo json_encode($loadedBook);
+        } else {
             $books = Book::loadAllBooks($mysql);
             echo (json_encode($books));
-            }
+        }
         break;
     
     case 'PUT':
-           // edit action;
-            parse_str(file_get_contents("php://input"), $put_vars);
-            $id=$put_vars['id'];
-            $loadedbook = Book::loadBookFromDBById ($mysql,$id);
-            $loadedbook->setTitle($put_vars['title']);
-            $loadedbook->setAuthor($put_vars['author']);
-            if ($loadedbook->saveBookInDB($mysql)) {
-                $books = Book::loadAllBooks($mysql);
-                echo (json_encode($books));
-            }
+        // edit action;
+        parse_str(file_get_contents("php://input"), $put_vars);
+        $id = $put_vars['id'];
+        $loadedbook = Book::loadBookFromDBById ($mysql,$id);
+        $loadedbook->setTitle($put_vars['title']);
+        $loadedbook->setAuthor($put_vars['author']);
+        if ($loadedbook->saveBookInDB($mysql)) {
+            $books = Book::loadAllBooks($mysql);
+            echo (json_encode($books));
+        }
         break;
         
     case 'DELETE':
-            //delete action;
-            parse_str(file_get_contents("php://input"), $del_vars);
-            $id=$del_vars['id'];
-            $delbook = Book::loadBookFromDBById ($mysql,$id);
-            if ($delbook->deleteBookFromDB($mysql)) {
-                $books = Book::loadAllBooks($mysql);
-                echo (json_encode($books));
-            }
-        break;
-    
+        //delete action;
+        parse_str(file_get_contents("php://input"), $del_vars);
+        $id = $del_vars['id'];
+        $delbook = Book::loadBookFromDBById ($mysql,$id);
+        if ($delbook->deleteBookFromDB($mysql)) {
+            $books = Book::loadAllBooks($mysql);
+            echo (json_encode($books));
+        }
+        break;    
 }
 
 
